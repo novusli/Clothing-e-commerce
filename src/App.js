cc344dccbebfee13e9b14f7c383d96310a781f5c
novusli <novuslzq@gmail.com>
2020-03-13 23:54:07 -0700

    Added header component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
     return (
         <div>
+        <Header />
         {/* Switch means ONLY render one of the Route inside (follow top-to-bottom order). */}
             <Switch>
                 {/* exact means if the path to be exactly same, not just including. 
